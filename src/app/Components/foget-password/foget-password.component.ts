f79de68982751163cecfd73008c924337ca68b20
Chaten Mundle <chetanmundle7875@gmail.com>
2024-10-28 14:40:10 +0530

    update for loader

--- src/app/Components/foget-password/foget-password.component.ts
+++ src/app/Components/foget-password/foget-password.component.ts
@@ -9,11 +9,12 @@ import { MustMatch } from '../register/CustomeValidation/MustMatch';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../Services/UserService/user.service';
 import { MyToastServiceService } from '../../Services/MyToastService/my-toast-service.service';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-foget-password',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, LoaderComponent],
   templateUrl: './foget-password.component.html',
   styleUrl: './foget-password.component.css',
 })
@@ -25,6 +26,8 @@ export class FogetPasswordComponent {
   private userService = inject(UserService);
   private tostr = inject(MyToastServiceService);
   private router = inject(Router);
+
+  isLoader: boolean = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.forgetForm = this.formBuilder.group(
@@ -62,6 +65,7 @@ export class FogetPasswordComponent {
   }
 
   onClickUpdateBtn() {
+    this.isLoader = true;
     let Data = this.forgetForm.value;
     if (Data.newPassword !== Data.confirmNewPassword) {
       alert('New Password and ConfirmNewPassword Must be same');
@@ -76,6 +80,7 @@ export class FogetPasswordComponent {
         } else {
           console.log('Unable to update Password');
         }
+        this.isLoader = false;
       },
       error: (error) => {
         if (error.status == 404) {
@@ -85,6 +90,7 @@ export class FogetPasswordComponent {
         } else {
           this.tostr.showError('Unable to update Password');
         }
+        this.isLoader = false;
       },
     });
   }
--- src/app/Components/register/register.component.ts
+++ src/app/Components/register/register.component.ts
@@ -10,11 +10,13 @@ import { MustMatch } from './CustomeValidation/MustMatch';
 import { Router, RouterLink } from '@angular/router';
 import { UserService } from '../../Services/UserService/user.service';
 import { MyToastServiceService } from '../../Services/MyToastService/my-toast-service.service';
+import { CommonModule } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
+  imports: [ReactiveFormsModule, RouterLink, CommonModule, LoaderComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
@@ -27,6 +29,10 @@ export class RegisterComponent {
   private userService = inject(UserService);
   private router = inject(Router);
   private tostr = inject(MyToastServiceService);
+
+  passwordStrength: string = '';
+  isFocucedonPass: boolean = false;
+  isLoader: boolean = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.userFormData = this.formBuilder.group(
@@ -65,6 +71,7 @@ export class RegisterComponent {
   }
 
   onClickRegister() {
+    this.isLoader = true;
     let user = this.userFormData.value;
     console.log('User : ', user);
 
@@ -84,6 +91,7 @@ export class RegisterComponent {
         } else {
           console.log('Unable to log in');
         }
+        this.isLoader = false;
       },
       error: (error) => {
         if (error.status == 409) {
@@ -92,7 +100,54 @@ export class RegisterComponent {
         }
         console.log('unable to Create User');
         console.log(error);
+        this.isLoader = false;
       },
     });
+  }
+
+  PrintStrongNess(event: any) {
+    let input_string = event.target.value;
+    const n = input_string.length;
+    // Checking lower alphabet in string
+    let hasLower = false;
+    let hasUpper = false;
+    let hasDigit = false;
+    let specialChar = false;
+    const normalChars =
+      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 ';
+
+    for (let i = 0; i < n; i++) {
+      if (input_string[i] >= 'a' && input_string[i] <= 'z') {
+        hasLower = true;
+      }
+      if (input_string[i] >= 'A' && input_string[i] <= 'Z') {
+        hasUpper = true;
+      }
+      if (input_string[i] >= '0' && input_string[i] <= '9') {
+        hasDigit = true;
+      }
+      if (!normalChars.includes(input_string[i])) {
+        specialChar = true;
+      }
+    }
+
+    // Strength of password
+    this.passwordStrength = 'Weak';
+
+    if (hasLower && hasUpper && hasDigit && specialChar && n >= 8) {
+      this.passwordStrength = 'Strong';
+    } else if ((hasLower || hasUpper) && specialChar && n >= 6) {
+      this.passwordStrength = 'Moderate';
+    }
+
+    console.log(`Strength of password: ${this.passwordStrength}`);
+  }
+
+  onPasswordBlur() {
+    this.isFocucedonPass = false;
+  }
+
+  onPasswordFocus() {
+    this.isFocucedonPass = true;
   }
 }
